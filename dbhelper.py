f1f6a1bb8b9229546d33dcfee683d4fa482b1b02
Kuldip Bhimani <108011984+kuldipbhimani@users.noreply.github.com>
2022-08-26 11:11:49 +0530

    Add files via upload

---
+++ dbhelper.py
@@ -0,0 +1,34 @@
+# database related logic dbhelper file ma lakhyu chhe
+
+import mysql.connector
+import sys
+
+class DBhelper:
+    def __init__(self):
+        try:
+            self.conn = mysql.connector.connect(host = "localhost", user = "root", password = "", database = "kuldip")
+            self.mycursor = self.conn.cursor()
+        except:
+            print("Some error occured. Could not connect to database")
+            sys.exit(0)
+        else:
+            print("Connected to Database")
+
+    def register(self,name,email,password):
+        try:
+            self.mycursor.execute("""
+            INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES (NULL, '{}', '{}', '{}');
+            """.format(name,email,password))
+            self.conn.commit()
+        except:
+            return -1
+        else:
+            return 1
+
+    def search(self,email,password):
+        self.mycursor.execute("""
+        SELECT * FROM users WHERE email LIKE '{}' AND password LIKE '{}'
+        """.format(email,password))
+
+        data = self.mycursor.fetchall()
+        return data
